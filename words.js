93d68becc4f1bbf4aeb28e28d068afda126bcf34
sharavak <86647111+sharavak@users.noreply.github.com>
2021-10-22 20:11:28 +0530

    Update words.js

--- words.js
+++ words.js
@@ -1,9 +1,8 @@
 const wordList = ()=> {
     let words = {
-        Fruit: ['Abiu', 'Açaí', 'Acerola', 'Ackee', 'African cucumber', 'Apple', 'Apricot', 'Avocado', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant',
-            'Blueberry', 'Boysenberry', 'Breadfruit', 'Canistel', 'Cempedak', 'Cherry', 'Cloudberry', 'Coconut', 'Cranberry', 'Currant', 'Damson', 'Date', 'Dragonfruit', 'Durian',
+        Fruit: ['Abiu', 'Açaí', 'Acerola', 'Ackee', 'Apple', 'Apricot', 'Avocado', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Canistel', 'Cempedak', 'Cherry', 'Coconut', 'Cranberry', 'Currant', 'Damson', 'Date', 'Dragonfruit', 'Durian',
             'Elderberry', 'Feijoa', 'Fig','Gooseberry', 'Grape', 'Raisin', 'Grapefruit','Grewia', 'Guava', 'Hala Fruit', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Jostaberry',
-            'Jujube', 'Kiwano', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loganberry', 'Longan', 'Loquat', 'Lulo', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Cantaloupe', 'Honeydew', 'Watermelon',  'Mulberry', 'Nance', 'Nectarine', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Pawpaw', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Prune (dried plum)', 'Pineapple', 'Pineberry', 'Plumcot', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Redcurrant', 'Salak', 'Satsuma','Soursop', 'Strawberry', 'Surinam cherry', 'Tamarillo', 'Tamarind', 'Tangelo', 'Tayberry', 'Yuzu', 'Cucumber', 'Eggplant', 'Jalapeno', 'Olive', 'Pea', 'Pumpkin', 'Squash', 'Tomato', 'Zucchini'],
+            'Jujube', 'Kiwano', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Longan', 'Loquat', 'Lulo', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Cantaloupe', 'Honeydew', 'Watermelon',  'Mulberry', 'Nance', 'Nectarine', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Pawpaw', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum','Pineapple', 'Pineberry', 'Plumcot', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Redcurrant', 'Salak', 'Satsuma','Soursop', 'Strawberry','Tamarillo', 'Tamarind', 'Tangelo', 'Tayberry', 'Yuzu', 'Cucumber', 'Eggplant', 'Jalapeno', 'Olive', 'Pea', 'Pumpkin', 'Squash', 'Tomato', 'Zucchini'],
         Vegetable: ['carrot', 'eggplant', 'cabbage', 'cucumber'],
         Animal: ['tiger', 'lion', 'deer', 'cheetah']
     };
