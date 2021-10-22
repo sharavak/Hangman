7a5ff766441bd31a1972b0167a5a488200e48895
sharavak <86647111+sharavak@users.noreply.github.com>
2021-10-22 20:00:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,14 +79,13 @@ show.addEventListener('keydown', function (e) {
                        }
                     }  
             }else if (c === images.length - 1) {
-                    img.src = `Images/${images[c]}`;
                     win.parentElement.classList.add('winners');
                     win.innerHTML = `<p class="win">You Lost the game</p><p class="win">The Word is ${word}`;
                     }else {
                     show.nextElementSibling.style.display = 'inline-block';
                     prev.textContent+=check+','
                     guess.textContent = `No of Guess:${noOfGuess += 1}`;
-                    img.src = images[c]
+                    img.src = `Images/${images[c]}`;
                     c = c + 1
                 }
             e.preventDefault()
